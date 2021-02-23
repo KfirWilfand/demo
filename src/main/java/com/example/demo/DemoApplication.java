70500da4b457094c8b3af4796254ddd697ee0c03
i521652 <kfir.wilfand@sap.com>
2021-02-23 16:16:00 +0200

    remove redundant

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,11 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    public static final String DUMMY_EMP = "/rest/emp/dummy";
-    public static final String GET_EMP = "/rest/emp/{id}";
-    public static final String GET_ALL_EMP = "/rest/emps";
-    public static final String CREATE_EMP = "/rest/emp/create";
-    public static final String DELETE_EMP = "/rest/emp/delete/{id}";
 //    public static IDB db;
     public static ReportController reportController;
 
